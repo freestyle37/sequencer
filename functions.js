5458e3187a702153d5bfe70920b5d16cad5bf252
freestyle37 <mapryaz@gmail.com>
2023-04-23 15:13:31 +0600

    playing an array of sounds

--- functions.js
+++ functions.js
@@ -3,12 +3,39 @@
 document.addEventListener("DOMContentLoaded", function() {
   //console.log(audio_clap1);
   //infinity();
+
+  //let kick = new Audio('./resources/wav/kick/ZDRAVSTVUITE.wav');
+  //let hat = new Audio('./resources/wav/hat/HAT.wav');
+  //let clap = new Audio('./resources/wav/clap/EGOIST.wav');
+  //let snare = new Audio('./resources/wav/snare/HOOKAH.wav');
+  //let audio_clap1 = new Audio('/resources/wav/clap/P - NNF V2 CLAP 5.wav');
+
+  const aud_pth = "./resources/wav";
+
+  const kick_url = aud_pth + "/kick/ZDRAVSTVUITE.wav";
+  const clap_url = aud_pth + "/clap/EGOIST.wav";
+  const hat_url = aud_pth + "/hat/HAT.wav";
+  const snare_url = aud_pth + "/snare/HOOKAH.wav";
+
+  const arr = [[kick_url, 100], [hat_url, 300], [clap_url, 500, 1000], [hat_url, 700]];
+
+  window.addEventListener('click', function(event) {
+
+    arr.forEach((item, i)=> {
+      //console.log(item[0], ' ', item[1], event.timeStamp);
+
+      setTimeout(() => { play(arr[i][0], arr[i][2]); }, arr[i][1]);
+    });
+    //event.timeStamp
+    
+    //clap.play();
+  });
+
   
+
 });
 
-window.addEventListener('click', function(){
-  play('./resources/wav/clap/EGOIST.wav', 3000);
-});
+
 document.addEventListener('keydown', keyDownHandler);
 document.addEventListener('keyup', keyUpHandler);
 
@@ -65,15 +92,13 @@ function infinity() {
 
 }
 
-function play( audio_path, time_in_milisec) {
+function play( audio_path, time_in_milisec=false) {
   let beep = new Audio( audio_path);
-  beep.loop = true;
+  //console.log(time_in_milisec);
+  //audio_obj_type.loop = true;
   beep.play();
-  setTimeout(() => { beep.pause(); }, time_in_milisec);
-}
+  if (time_in_milisec) {
+    setTimeout(() => { beep.pause(); }, time_in_milisec);
+  }
 
-let kick = new Audio('./resources/wav/kick/ZDRAVSTVUITE.wav');
-let hat = new Audio('./resources/wav/hat/HAT.wav');
-let clap = new Audio('./resources/wav/clap/EGOIST.wav');
-let snare = new Audio('./resources/wav/snare/HOOKAH.wav');
-//let audio_clap1 = new Audio('/resources/wav/clap/P - NNF V2 CLAP 5.wav');
+}
