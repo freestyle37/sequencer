51e88da7a4799f953efa0c9af141fafde4f4005b
freestyle37 <mapryaz@gmail.com>
2023-04-27 16:30:26 +0600

    html to php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8">
+    <link rel="icon" href="favicon.ico">
     <script src="./functions.js" charset="utf-8"></script>
     <title>Lesson</title>
   </head>
@@ -9,7 +10,7 @@
 
 <p>Focus the IFrame first (e.g. by clicking in it), then try pressing some keys.</p>
 <p id="log"></p>
-
+<?php echo 'zdarova'; ?>
 
   <style>
 
