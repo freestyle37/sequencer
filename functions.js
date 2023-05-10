0e52a1d86ce833609b20aace49ec43267173cd45
freestyle37 <mapryaz@gmail.com>
2023-05-10 23:56:33 +0600

    handler, handlers and a million more handlers

--- functions.js
+++ functions.js
@@ -1,37 +1,129 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function() {
-  //console.log(audio_clap1);
-  //infinity();
+let kick = new Audio('./resources/wav/kick/ZDRAVSTVUITE.wav');
+kick.volume = 0.5;
+let hat = new Audio('./resources/wav/hat/HAT.wav');
+let clap = new Audio('./resources/wav/clap/EGOIST.wav');
+let snare = new Audio('./resources/wav/snare/HOOKAH.wav');
 
-  //let kick = new Audio('./resources/wav/kick/ZDRAVSTVUITE.wav');
-  //let hat = new Audio('./resources/wav/hat/HAT.wav');
-  //let clap = new Audio('./resources/wav/clap/EGOIST.wav');
-  //let snare = new Audio('./resources/wav/snare/HOOKAH.wav');
-  //let audio_clap1 = new Audio('/resources/wav/clap/P - NNF V2 CLAP 5.wav');
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+
+
+  let screenLog = document.querySelector("#screen-log");
+
+  let s = document.querySelector('.s');
+
+  let drums_ui = document.createElement('div');
+  drums_ui.setAttribute('class', 'drums_ui');
+  s.append(drums_ui);
+
+  let drums_list = createItem('drums_list');
+  drums_ui.append(drums_list);
+
+  let drums_list_add_item_btn = createItem('drums_list_add_item_btn', 'add');
+  drums_ui.append(drums_list_add_item_btn);
 
   const aud_pth = "./resources/wav";
 
-  const kick_url = aud_pth + "/kick/ZDRAVSTVUITE.wav";
-  const clap_url = aud_pth + "/clap/EGOIST.wav";
-  const hat_url = aud_pth + "/hat/HAT.wav";
-  const snare_url = aud_pth + "/snare/HOOKAH.wav";
+  const urls = {
+    kick_url: aud_pth + "/kick/ZDRAVSTVUITE.wav",
+    clap_url: aud_pth + "/clap/EGOIST.wav",
+    hat_url: aud_pth + "/hat/HAT.wav",
+    snare_url: aud_pth + "/snare/HOOKAH.wav"
+  }
 
-  const arr = [[kick_url, 100], [hat_url, 300], [clap_url, 500, 1000], [hat_url, 700]];
+  const arr = [[urls.kick_url, 100], [urls.hat_url, 300], [urls.clap_url, 500, 1000], [urls.hat_url, 700]];
 
   window.addEventListener('click', function(event) {
 
     arr.forEach((item, i)=> {
       //console.log(item[0], ' ', item[1], event.timeStamp);
 
-      setTimeout(() => { play(arr[i][0], arr[i][2]); }, arr[i][1]);
+      //setTimeout(() => { play(arr[i][0], arr[i][2]); }, arr[i][1]);
     });
     //event.timeStamp
     
     //clap.play();
+
   });
 
+  let s_head = document.querySelector('.s_head');
+  const draggable = document.querySelectorAll('.draggable');
   
+  draggable.forEach(function(element) {
+
+      
+      element.onmousedown = function() {
+        return false;
+      };
+      element.addEventListener('mousedown', sHeadMousedownHandler);
+    
+
+  });
+
+
+  function sHeadMousedownHandler(event) {
+    if (!event.target) {
+      return;
+    }
+   if (event.button !== 0) {
+     //console.log(event.target);
+     return
+    } else {
+      let s_head_rect = s_head.getBoundingClientRect();
+
+      const cusrsor_init = {
+        x: event.pageX,
+        y: event.pageY
+      }
+
+      //console.log(s_head_rect);
+  
+      let my_event = new Event("delete-up-and-move");
+      
+
+      const sMove = { handleEvent: sHeadMouseMoveHandler, s_head_rect, cusrsor_init};
+
+      const sUp = { handleEvent: sHeadMouseUpHandler, my_event: my_event };
+
+      const sThree = { handleEvent: deleteMoveAndUpHandler, sMove, sUp: sUp };
+
+      document.addEventListener("mousemove", sMove);
+      document.addEventListener("mouseup", sUp);
+      document.addEventListener("delete-up-and-move", sThree);
+
+    }
+  }
+
+  function sHeadMouseMoveHandler(event) {
+    screenLog.innerText = `${event.pageX} ${event.pageY}`;
+    s.style.left = this.s_head_rect.left + (event.pageX - this.cusrsor_init.x) + 'px';
+    s.style.top = this.s_head_rect.top + (event.pageY - this.cusrsor_init.y) + 'px';
+
+    console.log(this.s_head_rect.left, this.s_head_rect.top, this.cusrsor_init.x, this.cusrsor_init.y);
+  }
+
+  function sHeadMouseUpHandler(event) {
+    //console.log(this.my_event);
+    document.dispatchEvent(this.my_event);
+    //deleteMoveAndUpHandler(this.sMove, this.sUp);
+    
+  }
+
+  function deleteMoveAndUpHandler(event) {
+    console.log('d');
+    document.removeEventListener("mousemove", this.sMove);
+    document.removeEventListener("mouseup", this.sUp);
+  }
+
+  drums_list_add_item_btn = document.querySelector('.drums_list_add_item_btn');
+  this.addEventListener('click', function() {
+
+  });
 
 });
 
@@ -42,19 +134,20 @@ document.addEventListener('keyup', keyUpHandler);
 
 
 function keyDownHandler(event) {
+  if (event.repeat) { return }
   console.log(event.code);
 
   if (event.code === 'KeyQ') {
+    //console.log(kick);
     kick.play();
-  }
-  if (event.code === 'KeyW') {
+  } else if (event.code === 'KeyW') {
     hat.play();
-  }
-  if (event.code === 'KeyE') {
+  } else if (event.code === 'KeyE') {
     clap.play();
-  }
-  if (event.code === 'KeyR') {
+  } else if (event.code === 'KeyR') {
     snare.play();
+  } else {
+    return;
   }
 }
 
@@ -79,26 +172,25 @@ function keyUpHandler(event) {
   }
 }
 
-function infinity() {
-  const arr = ['q', 'w', 'e', 'e'];
-  arr.forEach(function(item) {
-    if (item === 'q') {
-      setTimeout(() => kick.play(), 1000);
-    }
-    if (item === 'w') {
-      setTimeout(() => hat.play(), 1000);
-    }
-  });
-
-}
-
 function play( audio_path, time_in_milisec=false) {
   let beep = new Audio( audio_path);
   //console.log(time_in_milisec);
-  //audio_obj_type.loop = true;
+  //audio_obj_type.loop = true; 
   beep.play();
   if (time_in_milisec) {
     setTimeout(() => { beep.pause(); }, time_in_milisec);
   }
 
 }
+
+function createItem(class_name, item_txt) {
+  let item = document.createElement('div');
+  item.setAttribute('class', class_name);
+
+  if (item_txt) {
+    item.textContent = item_txt;
+  }
+
+  return item;
+}
+
--- index.php
+++ index.php
@@ -2,15 +2,30 @@
 <html>
   <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="./styles.css">
     <link rel="icon" href="favicon.ico">
     <script src="./functions.js" charset="utf-8"></script>
     <title>Lesson</title>
   </head>
   <body>
 
-<p>Focus the IFrame first (e.g. by clicking in it), then try pressing some keys.</p>
-<p id="log"></p>
-<?php echo 'zdarova'; ?>
+<p id="screen-log"></p>
+
+<div class="main">
+  <!--div class="sidebar">
+    <h4>instruments</h4>
+  </div-->
+
+  <div class="s draggable">
+    <div class="s_head">drums_ui</div>
+  </div>
+
+  <div class="modal draggable">
+    <div class="modal_head">instruments</div>
+  </div>
+
+</div>
+
 
   <style>
 
